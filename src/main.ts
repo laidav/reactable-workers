8b82e6b94956444146fcefe2057754dbeb534424
David Lai <laidav@gmail.com>
2025-01-26 22:07:39 -0500

    move tempalte to index

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
 import "./style.css";
 import { Action } from "@reactables/core";
-import typescriptLogo from "./typescript.svg";
-import viteLogo from "/vite.svg";
 import { fromWorker } from "./fromWorker.ts";
 import { RxToggle, ToggleState, ToggleActions } from "./RxToggle.ts";
 import { setupCounter } from "./counter.ts";
@@ -28,27 +26,6 @@ state$.subscribe((state) => {
 (actions$ as Observable<Action<unknown>>).subscribe((action) => {
   console.log(action);
 });
-
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="toggle" type="button">Toggle</button>
-    </div>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`;
 
 document
   .getElementById("toggle")
